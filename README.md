dba524305cf713c421ad7722949b077a71b0a475
wobsoriano <sorianorobertc@gmail.com>
2025-05-30 23:50:34 -0700

    chore: rely on nestjs config for initial clerk client config

--- README.md
+++ README.md
@@ -10,18 +10,26 @@ npm install nestjs-clerk
 
 ## Usage
 
+Retrieve your Backend API key from the [API Keys](https://dashboard.clerk.com/last-active?path=api-keys) screen in your Clerk dashboard and set it as an environment variable in a .env file:
+
+```sh
+CLERK_PUBLISHABLE_KEY=pk_*******
+CLERK_SECRET_KEY=sk_******
+```
+
 Import the `ClerkModule` into the root module and configure it with your Clerk API keys.
 
 ```ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ClerkModule } from 'nestjs-clerk';
 
 @Module({
   imports: [
-    ClerkModule.forRoot({
-      publishableKey: 'pk_***',
-      secretKey: 'sk_***',
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    ClerkModule.forRoot(),
   ],
 })
 export class AppModule {}
@@ -36,7 +44,9 @@ import { ClerkClient } from '@clerk/backend';
 
 @Injectable()
 export class AppService {
-  public constructor(@InjectClerkClient() private readonly clerkClient: ClerkClient) {
+  public constructor(
+    @InjectClerkClient() private readonly clerkClient: ClerkClient
+  ) {
     async getUser(): string {
       const user = await this.clerkClient.users.getUser('user_id');
       return `Hello, ${user.fullName}!`;
--- src/clerk-core.module.ts
+++ src/clerk-core.module.ts
@@ -1,14 +1,34 @@
 import { type DynamicModule, Global, Module, type Provider } from '@nestjs/common';
 import { CLERK_CLIENT_TOKEN } from './clerk.constants';
-import { type ClerkClient, type ClerkOptions, createClerkClient } from '@clerk/backend';
+import { type ClerkOptions, createClerkClient } from '@clerk/backend';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({})
 export class ClerkCoreModule {
-  public static forRoot(options: ClerkOptions): DynamicModule {
-    const clerkClient: Provider<ClerkClient> = {
+  public static forRoot(options?: ClerkOptions): DynamicModule {
+    const clerkClient: Provider = {
       provide: CLERK_CLIENT_TOKEN,
-      useValue: createClerkClient(options),
+      useFactory: (configService: ConfigService) => {
+        return createClerkClient({
+          publishableKey:
+            options?.publishableKey ??
+            configService.get('CLERK_PUBLISHABLE_KEY'),
+          secretKey:
+            options?.secretKey ?? configService.get('CLERK_SECRET_KEY'),
+          jwtKey: options?.secretKey ?? configService.get('CLERK_JWT_KEY'),
+          apiUrl:
+            options?.apiUrl ??
+            configService.get('CLERK_API_URL') ??
+            'https://api.clerk.com',
+          apiVersion:
+            options?.apiVersion ??
+            configService.get('CLERK_API_VERSION') ??
+            'v1',
+          domain: options?.domain ?? configService.get('CLERK_DOMAIN') ?? '',
+        });
+      },
+      inject: [ConfigService],
     };
 
     return {
